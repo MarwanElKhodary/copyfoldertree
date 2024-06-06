0e34ea95223c2386d51df8602f1d9686bf72099e
Marwan ElKhodary <marwan.elkhodary@gmail.com>
2024-06-05 21:25:01 -0400

    feat: Get the path where the user right clicked

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,13 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-	
-	const disposable = vscode.commands.registerCommand('copyfoldertree.copy', () => {
-		vscode.window.showInformationMessage('Hello World from CopyFolderTree!');
+
+	const disposable = vscode.commands.registerCommand('copyfoldertree.copy', (resourceUri: vscode.Uri) => {
+		if (resourceUri) {
+			const parentPath = resourceUri.fsPath;
+		} else {
+			vscode.window.showWarningMessage('No resource selected!');
+		}
 	});
 
 	context.subscriptions.push(disposable);
